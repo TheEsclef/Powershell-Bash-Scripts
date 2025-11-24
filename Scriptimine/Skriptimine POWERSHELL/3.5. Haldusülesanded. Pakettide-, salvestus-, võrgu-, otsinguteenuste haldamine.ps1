fd4da9ccf7d17cb1b9714e83cadf4160b2f7923c
TheEsclef <108223871+TheEsclef@users.noreply.github.com>
2025-11-24 10:12:50 +0200

    Add files via upload

---
+++ Scriptimine/Skriptimine POWERSHELL/3.5. Haldusülesanded. Pakettide-, salvestus-, võrgu-, otsinguteenuste haldamine.ps1
@@ -0,0 +1,18 @@
+﻿#Võtab CPU kasutus protsendi
+$cpu = (get-ciminstance -Class Win32_Processor | Measure-Object -Property LoadPercentage -Average).Average
+
+#Võtab RAMi kasutus protsendi
+$os = Get-CimInstance Win32_OperatingSystem
+$totalRAM = $os.TotalVisibleMemorySize
+$freeRAM = $os.FreePhysicalMemory
+$ram = [math]::Round((($totalRAM - $freeRAM) / $totalRAM) * 100,2) 
+
+#Võtab ketta kasutus protsendi
+$drivename = Get-PSDrive "C"
+$drive = [math]::Round(($drivename.Used / ($drivename.Used + $drivename.Free)) * 100)
+
+Set-Content -Path "C:\Users\Kardo.E\Desktop\logs\monitor.txt" -Value @(
+    "CPU: $($cpu) %"
+    "RAM: $($ram) %"
+    "DISK: $($drive) %"
+)
